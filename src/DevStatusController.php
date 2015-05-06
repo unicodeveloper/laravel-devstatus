808962348c32a653f265e797cb245ce3141490ae
busayo <prosperotemuyiwa@gmail.com>
2015-05-06 17:20:43 +0100

    Initial Commit

---
+++ src/DevStatusController.php
@@ -0,0 +1,50 @@
+<?php namespace Busayo\DevStatus;
+use App\Http\Controllers\Controller;
+use App\User;
+
+class DevStatusController extends Controller {
+
+  /**
+  * Display an object with a Welcome Status
+  *
+  * @return Response
+  */
+  public function home(){
+    return response([
+        'Developer Status' => 'Welcome to Dev Status Laravel 5 package'
+    ]);
+  }
+
+  /**
+  * Display developer status based on the number of Github Repos
+  *
+  * @return Response
+  */
+  public function index($username)
+  {
+
+    $options      = array('http' => array('user_agent'=> config('DevStatus.user-agent')));
+    $context      = stream_context_create($options);
+    $url          = config('DevStatus.url') . "/users/" . urlencode($username);
+    $result       = json_decode(file_get_contents($url, true, $context));
+    $name         = $result->name;
+    $public_repos = $result->public_repos;
+    $status       = "";
+
+    if($public_repos <= 10){
+       $status = "Rookie";
+    }
+    if( $public_repos > 10 && $public_repos <= 25){
+       $status = "Intermediate";
+    }
+    if( $public_repos > 25){
+       $status = "Ninja";
+    }
+
+    $finalStatus = $name . " is a " . $status . " Open Source Evangelist";
+
+    return response([
+        'Developer Status' => $finalStatus
+    ]);
+  }
+}
---
+++ src/DevStatusServiceProvider.php
@@ -0,0 +1,32 @@
+<?php namespace Busayo\DevStatus;
+
+use Illuminate\Support\ServiceProvider;
+
+class DevStatusServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//Publishes package config file to applications config folder
+		$this->publishes([__DIR__.'/config/DevStatus.php' => config_path('DevStatus.php')]);
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//Register Our Package routes
+		include __DIR__.'/routes.php';
+
+		// Let Laravel Ioc Container know about our Controller
+		$this->app->make('Busayo\DevStatus\DevStatusController');
+	}
+
+}
