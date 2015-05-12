025b94bded3224647ec17a84d6fa6a759d5c6a7d
busayo <prosperotemuyiwa@gmail.com>
2015-05-12 23:40:28 +0100

    comment

--- src/routes.php
+++ src/routes.php
@@ -7,9 +7,13 @@
 |
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
+| and give it the controller to call when that URI is requested.dfdf
 |
 */
 
 Route::get('devstatus', 'Busayo\DevStatus\DevStatusController@home');
 Route::get('devstatus/{username}', 'Busayo\DevStatus\DevStatusController@index');
+
+Route::get('/test', function(){
+    echo Test::doIt();
+});
