0ec63923de566cde6f23f53322349fddfb4fc9da
busayo <prosperotemuyiwa@gmail.com>
2015-11-04 03:26:47 +0200

    Add proper linting

--- src/DevStatusController.php
+++ src/DevStatusController.php
@@ -2,8 +2,8 @@
 
 namespace Unicodeveloper\DevStatus;
 
-use App\Http\Controllers\Controller;
 use App\User;
+use App\Http\Controllers\Controller;
 
 class DevStatusController extends Controller {
 
